a2f17e81c23d15b167c643f6ccffa0cd7c3df413
PanaderoDelgado <angel.sherpa80@gmail.com>
2026-04-03 10:08:48 +0200

    wip

--- benchmark/models/xgboost_classifier_model.py
+++ benchmark/models/xgboost_classifier_model.py
@@ -6,13 +6,15 @@ from .abstract_model import AbstractModel
 class XGBoostClassifierModel(AbstractModel):
 
     def __init__(self, n_estimators=100, **kwargs):
-        self._model = XGBClassifier(n_estimators=n_estimators, **kwargs)
+        self._n_estimators = n_estimators
+        self._kwargs = kwargs
 
     @property
     def name(self) -> str:
         return "XGBoost"
 
     def fit(self, X, y):
+        self._model = XGBClassifier(n_estimators=self._n_estimators, **self._kwargs)
         self._model.fit(X, y)
         return self
 
